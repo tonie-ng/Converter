9a1fc9319879ec41202289421367b4e839c70d56
Tonie <victornwanochi@gmail.com>
2023-03-21 09:08:25 +0100

    feat: worked extensively on teh gateway service. Implemented token validation

--- src/gateway/server.py
+++ src/gateway/server.py
@@ -1,6 +1,8 @@
 import os, gridfs, pika, json
 from flask import Flask, request
 from flask_pymongo import PyMongo
+from auth_svc import access
+from auth import validate
 
 
 server = Flask(__name__)
@@ -9,3 +11,20 @@ server.config['MONGO_URI'] = os.environ['MONGO_URI']
 mongo = PyMongo(server)
 
 fs = gridfs.GridFS(mongo.db)
+
+connection = pika.BlockingConnection(pika.ConnectionParameters("rabbitmq"))
+channel = connection.channel()
+
+
+@server.route('/login', methods=['POST'])
+def login():
+    token, err = access.login(request)
+
+    if not err:
+        return token
+    else: 
+        return err
+    
+@server.route('/upload', methods=['POST'])
+def upload():
+    access, err = validate.token(request)
